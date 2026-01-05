e43816bba03b1110de7a15b489edf4fb7fa10db0
giovannimazzi <gioelor@libero.it>
2026-01-05 11:31:28 +0100

    Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero.

--- main.js
+++ main.js
@@ -14,12 +14,11 @@ if (!n || n < 0) {
   alert(msg);
   throw new Error(msg);
 } else {
-  console.log(`N: ${n}`);
   const cubeN = [];
   for (let i = 1; i <= n; i++) {
     cubeN.push(i ** 3);
   }
-  console.table(cubeN);
+  console.log(`N: ${n} -> ${cubeN}`);
   alert(`N: ${n} -> ${cubeN}`);
 }
 
@@ -31,5 +30,30 @@ const pot2 = [];
 for (let p = 1; p <= 1000; p *= 2) {
   pot2.push(p);
 }
-console.table(pot2);
+console.log(`Potenze di 2 fino a 1000 -> ${pot2}`);
 alert(`Potenze di 2 fino a 1000 -> ${pot2}`);
+
+//* Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero.
+
+console.log(
+  `- Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero.`
+);
+const num = parseInt(
+  prompt(
+    `Calcolo della somma di tutte le cifre che compongono il numero.\n\nInserire il numero N desiderato (4 cifre):`
+  )
+);
+
+if (!num || num < 1000 || num > 9999) {
+  const msg = `Numero inserito (${num}) non valido! Esecuzione interrotta.`;
+  alert(msg);
+  throw new Error(msg);
+} else {
+  let somma = 0;
+  const numStr = String(num);
+  for (let i = 0; i < numStr.length; i++) {
+    somma += parseInt(numStr[i]);
+  }
+  console.log(`Somma: ${num} -> ${somma}`);
+  alert(`Somma: ${num} -> ${somma}`);
+}
